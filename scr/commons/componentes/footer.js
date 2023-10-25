1b9182ebecf80ac695ac739b86f29dc7b371de4a
JoaoBoscoLuizJr <joaoboscoluizjunior404@gmail.com>
2023-10-24 22:21:00 -0300

    atualizando e organizando pastas

---
+++ scr/commons/componentes/footer.js
@@ -0,0 +1,4 @@
+function footer() {
+    footerElement.innerHTML = `<footer class=" fixed-bottom  mt-4 text-center bg-white"> João Bosco</footer> `
+}
+footer();
---
+++ scr/commons/componentes/header.js
@@ -0,0 +1,4 @@
+function  header () {
+    headerElement.innerHTML  = `       `
+}
+header();
